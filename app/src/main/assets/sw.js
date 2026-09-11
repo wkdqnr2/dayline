80af19a569bd8a3c35d7620459deafd948be7f9d
이승재 <73099422+wkdqnr2@users.noreply.github.com>
2026-09-11 18:36:12 +0900

    Add service worker

---
+++ app/src/main/assets/sw.js
@@ -0,0 +1,3 @@
+const CACHE='dayline-v1'; const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
